b4ffb5295984d9bb573c1855061cb05b8eee25ae
Victor Calderon <victorcalderon@protonmail.com>
2023-04-06 19:16:49 -0400

    added all the smartpointer magic required

--- src/main.rs
+++ src/main.rs
@@ -1,100 +1,125 @@
+use std::cell::RefCell;
 use std::ops;
 use std::rc::Rc;
-use std::cell::RefCell;
 // use petgraph::graph::{Graph};
-
-
 type MutableRc<T> = Rc<RefCell<T>>;
 fn mutable_rc<T>(data: T) -> MutableRc<T> {
     Rc::new(RefCell::new(data))
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 enum Operation {
     Add,
-    Mul
+    Mul,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 struct Value {
     label: String,
     data: f64,
     operation: Option<Operation>,
-    children: Vec<MutableRc<Value>>
+    children: Vec<MutableRc<Value>>,
 }
 
-
 impl Value {
-    // Initializing new value 
+    // Initializing new value
     fn new(data: f64, label: String) -> Self {
-        Self { data, label, ..Value::default() }
+        Self {
+            data,
+            label,
+            ..Value::default()
+        }
     }
+
+    // // Add child
+    // fn add_child(&mut self, child: MutableRc<Value>) {
+    //     self.children.push(child);
+    // }
+    //
+    // // Simple depth first algorithm
+    // fn depth_first(&self) {
+    //     println!("Value {}", self.label);
+    //     for child in self.children.iter() {
+    //         child.borrow().depth_first();
+    //     }
+    // }
 }
 
 impl Default for Value {
     fn default() -> Value {
-        Value { label: "".to_owned(), data: 0., operation: None, children: vec![] }
+        Value {
+            label: "".to_owned(),
+            data: 0.,
+            operation: None,
+            children: vec![],
+        }
     }
 }
 
-impl ops::Add<Value> for Value {
+impl ops::Add<&Value> for &Value {
     type Output = Value;
 
-    fn add(self, other: Value) -> Value {
-        // New value label
-        let new_label = format!("{}{}", self.label, other.label);
-        let new_value = self.data + other.data;
-        Value { 
-            label: new_label, 
-            data: new_value, 
+    fn add(self, other: &Value) -> Value {
+        Value {
+            label: format!("{}{}", self.label, other.label),
+            data: self.data + other.data,
             operation: Some(Operation::Add),
-            ..Value::default()
+            children: vec![mutable_rc(self.to_owned()), mutable_rc(other.to_owned())],
         }
     }
 }
 
-impl ops::Mul<Value> for Value {
+impl ops::Mul<&Value> for &Value {
     type Output = Value;
 
-    fn mul(self, other: Value) -> Value {
-        // New value label
-        let new_label = format!("{}{}", self.label, other.label);
-        let new_value = self.data * other.data;
-        Value { 
-            label: new_label, 
-            data: new_value, 
-            operation: Some(Operation::Mul), 
-            ..Value::default()
+    fn mul(self, other: &Value) -> Value {
+        Value {
+            label: format!("{}{}", self.label, other.label),
+            data: self.data * other.data,
+            operation: Some(Operation::Mul),
+            children: vec![mutable_rc(self.to_owned()), mutable_rc(other.to_owned())],
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct ValueGraph {
-    values: Vec<Value>,
+    nodes: Vec<MutableRc<Value>>,
+    // edges: Vec<(Weak<RefCell<Value>>, MutableRc<Value>)>,
+    edges: Vec<(MutableRc<Value>, MutableRc<Value>)>,
 }
 
 impl ValueGraph {
-   // Create a new ValueGraph using nodes as input
-   fn new(values: Vec<Value>) -> Self {
-       Self { values }
-   }
-}
+    fn new() -> ValueGraph {
+        ValueGraph { nodes: vec![], edges: vec![] }
+    }
 
+    fn new_with_nodes(values: Vec<MutableRc<Value>>) -> ValueGraph {
+        let mut graph = Self::new();
+        for node in values {
+            graph._build(node);
+        }
+        return graph
+    }
+
+    fn _build(&mut self, node: MutableRc<Value>) {
+        if !self.nodes.contains(&node) { 
+           self.nodes.push(Rc::clone(&node));
+           for child in &node.borrow().children {
+                self.edges.push((Rc::clone(&node), Rc::clone(&child)));
+                self._build(Rc::clone(child));
+           }
+        }
+    }
+}   
 
 fn main() {
     let first_value = Value::new(5., "Hello".to_owned());
     let second_value = Value::new(10., "World".to_owned());
-
-    println!("This is value A: {:?}", first_value.clone());        
-    println!("This is value B: {:?}", second_value.clone());
-
-    let added_values = first_value.clone() + second_value.clone();
-
-    println!("This is the sum of those two values: {:?}", added_values.data);
-    println!("The operation performed was: {:?}", added_values.operation);
-    println!("The children of this node is: {:?}", added_values.children);
-
-    let my_graph = ValueGraph::new(vec![first_value.clone(), second_value.clone(), added_values]);
-    println!("This is my graph: {:?}", my_graph.values);
+    // Operator overload working on references
+    let third_value = &first_value + &second_value;
+    // Build a graph
+    let graph = ValueGraph::new_with_nodes(vec![mutable_rc(first_value), mutable_rc(second_value), mutable_rc(third_value)]);
+    // Let's see if it's true
+    dbg!(&graph);
 }
