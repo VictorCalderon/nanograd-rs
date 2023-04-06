3d5029acd13ece7bde3b4f6028e41f8d0e475bb6
Victor Calderon <victorcalderon@protonmail.com>
2023-04-06 11:04:11 -0400

    Added operations and parents to Value

--- src/main.rs
+++ src/main.rs
@@ -37,13 +37,33 @@ impl ops::Add<Value> for Value {
     }
 }
 
+impl ops::Mul<Value> for Value {
+    type Output = Value;
+
+    fn mul(self, other: Value) -> Value {
+        // New value label
+        let new_label = format!("{}{}", self.label, other.label);
+        let new_value = self.data * other.data;
+        Value { 
+            label: new_label, 
+            data: new_value, 
+            operation: Some(Operation::Mul), 
+            parents: Some(vec![self.label, other.label])
+        }
+    }
+}
+
 fn main() {
     let first_value = Value::new(5, "Hello".to_owned());
     let second_value = Value::new(10, "World".to_owned());
+
     println!("This is value A: {:?}", first_value);        
     println!("This is value B: {:?}", second_value);
 
     let added_values = first_value + second_value;
-    println!("This is the sum of those two values: {:?}", added_values);
+
+    println!("This is the sum of those two values: {:?}", added_values.data);
+    println!("The operation performed was: {:?}", added_values.operation);
+    println!("The parents of this node is: {:?}", added_values.parents);
     println!("Hello, world!");
 }
