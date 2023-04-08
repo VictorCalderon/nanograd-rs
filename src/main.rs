5d5708553af08f4ea985b705fcb6e6095fbcf2cd
Victor Calderon <victorcalderon@protonmail.com>
2023-04-08 13:40:53 -0400

    feat: fully traced nodes finally added

--- src/main.rs
+++ src/main.rs
@@ -73,41 +73,45 @@ fn format_node(node: &MutableRc<Value>) -> String {
     format!("{}({})", node.borrow().label.clone(), node.borrow().data)
 }
 
-// Plot helper function
-fn plot_graph(nodes: Vec<MutableRc<Value>>) {
-    // Iterate over nodes
-    let mut all_nodes = vec![];
-    let mut all_edges = vec![];
+type Node = MutableRc<Value>;
+type Edge = (Node, Node);
 
-    for node in nodes.iter() {
-        // Iterate over these previous nodes, format them, and add to edges
-        let formatted_nodes: Vec<(usize, String)> = node
-            .borrow()
-            .previous
-            .iter()
-            .map(|p| (p.borrow().id, format_node(p)))
-            .collect();
-        // Build edges
-        let edges: Vec<(usize, usize)> = formatted_nodes
-            .iter()
-            .map(|(id, _node)| (id.clone(), node.borrow().id))
-            .collect();
-        // Push both to their respective containers
-        all_nodes.extend(formatted_nodes);
-        all_nodes.extend(vec![(node.borrow().id, format_node(node))]);
-        all_edges.extend(edges);
+fn build(node: MutableRc<Value>, nodes: &mut Vec<Node>, edges: &mut Vec<Edge>) {
+    if nodes.contains(&node) {
+        return;
     }
-    // Create empty graph
-    let mut graph = Graph::<String, String, Directed, usize>::from_edges(&all_edges);
-    // Add node's value
-    for (id, node) in all_nodes.iter() {
-        let mutable_node = graph.node_weight_mut(NodeIndex::new(id.clone()));
-        if let Some(x) = mutable_node {
-            x.clear();
-            x.push_str(node);
+
+    nodes.push(Rc::clone(&node));
+    for child in node.borrow().previous.iter() {
+        edges.push((Rc::clone(&child), Rc::clone(&node)));
+        build(Rc::clone(child), nodes, edges);
+    }
+}
+
+fn trace(root: MutableRc<Value>) -> (Vec<Node>, Vec<Edge>) {
+    let mut nodes = vec![];
+    let mut edges = vec![];
+    build(root, &mut nodes, &mut edges);
+    (nodes, edges)
+}
+
+fn plot_graph(nodes: &Vec<Node>, edges: &Vec<Edge>) {
+    // Parse edges as a vector<usize, usize>
+    let parsed_edges: Vec<(usize, usize)> = edges
+        .iter()
+        .map(|e| (e.0.borrow().id, e.1.borrow().id))
+        .collect();
+    // Build a Graph
+    let mut graph = Graph::<String, String, Directed, usize>::from_edges(&parsed_edges);
+    // Iterate over nodes
+    for node in nodes.iter() {
+        let grab_node = graph.node_weight_mut(NodeIndex::new(node.borrow().id));
+        if let Some(n) = grab_node {
+            n.truncate(0);
+            n.push_str(&format_node(node));
         }
     }
-    // Plot graph
+    // Extend this graph with edges
     println!("{:?}", Dot::with_config(&graph, &[Config::EdgeNoLabel]));
 }
 
@@ -115,7 +119,10 @@ fn main() {
     let first_value = Value::new(5., "A".to_owned());
     let second_value = Value::new(10., "B".to_owned());
     let third_value = &first_value + &second_value;
-    let fourth_value = &third_value + &first_value;
-    // Plot this graph
-    plot_graph(vec![mutable_rc(fourth_value)]);
+    let fourth_value = Value::new(0.5, "C".to_owned());
+    let fifth_value = &fourth_value * &third_value;
+    let sixth_value = Value::new(10., "D".to_owned());
+    let seventh_value = &fifth_value + &sixth_value;
+    let (nodes, edges) = trace(mutable_rc(seventh_value));
+    plot_graph(&nodes, &edges);
 }
